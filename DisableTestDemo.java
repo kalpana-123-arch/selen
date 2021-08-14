a5387778da9e45b8cf859307a6123bb0c7bd7a72
kalpana <84218950+kalpana-123-arch@users.noreply.github.com>
2021-08-14 16:58:54 +0530

    Add files via upload

---
+++ DisableTestDemo.java
@@ -0,0 +1,21 @@
+package project1;
+
+import org.testng.annotations.Test;
+
+public class DisableTestDemo 
+{
+    @Test(enabled = true)
+    public void testMethodOne() {
+        System.out.println("Test method one.");
+    }
+ 
+    @Test(enabled = false)
+    public void testMethodTwo() {
+        System.out.println("Test method two.");
+    }
+ 
+    @Test
+    public void testMethodThree() {
+        System.out.println("Test method three.");
+    }
+}
